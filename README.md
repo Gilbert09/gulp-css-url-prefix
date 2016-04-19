0d6e60eac42ba82bb9af526d541d7dd08a348a11
Thomas Owers <thomas@hirespace.com>
2016-04-19 12:45:08 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # gulp-css-url-prefix
 Prefix urls with a matching pattern in CSS stylesheets
+
+## Install
+```
+npm install gulp-css-url-prefix
+```
+
+## Usage
+
+```
+urlPrefix(prefix, regexMatch)
+```
+`prefix` being what you want prefixed to the url
+`regexMatch` being which url you want to target. For example you may have `background-image: `
+
+```js
+var urlPrefix = require('gulp-css-url-prefix');
+
+gulp.task('css-assets', function() {
+  return gulp.src('src.css')
+    .pipe(urlPrefix('http://example.com', '\/assets'))
+    .pipe(gulp.dest('style.css'));
+});
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+
+// Consts
+var PLUGIN_NAME = 'gulp-css-url-prefix';
+
+function gulpCssUrlPrefix(prefix, regexMatch) {
+
+    return through.obj(function(file, enc, callback) {
+        if (file.isNull()) {
+            callback(null, file);
+        }
+
+        if (file.isBuffer()) {
+            var reg = new RegExp("url[(]['\"]?" + regexMatch + "([^'\")]*)['\"]?[)]", "g");
+
+            var contents = file.contents.toString('utf8');
+
+            file.contents = new Buffer(
+                contents.replace(reg, "url('" + prefix + "/$1')")
+            );
+        }
+
+        if (file.isStream()) {
+            return this.emit('error', new gutil.PluginError(PLUGIN_NAME,  'Streaming not supported'));
+        }
+
+        callback(null, file);
+    });
+}
+
+module.exports = gulpCssUrlPrefix;
